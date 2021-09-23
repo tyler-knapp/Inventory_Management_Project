a842d228b632ad1676040cd349b8589dc09323e7
tyler <tylerknapp25@gmail.com>
2021-09-23 10:30:28 -0400

    added instance variables

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.math.BigDecimal;
+
 public class Account {
+
+    private BigDecimal accountBalance;
+    private BigDecimal transactionAmount;
+    private String accountHolderName;
+
+
+
+
+    public Account(BigDecimal accountBalance, String accountHolderName){
+        this.accountBalance = accountBalance;
+        this.accountHolderName = accountHolderName;
+    }
+
+
+
 }
